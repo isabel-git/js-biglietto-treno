7d34fbfce8600203561dffe425ebe2284655abe7
Isabel <isabel.gogov1@libero.it>
2020-10-20 17:36:25 +0200

    condizioni di controllo del prezzo

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 var costoBase;
+var scontoUno;
+var scontoDue;
+var costoTot;
+
 // INPUT
   // chiedere all'utente il numero di km da percorrere
   var km = parseInt(prompt("inserisci il numero di chilometri da percorrere"));
-  console.log("kilometri ",km)
+  console.log("kilometri ",km);
 
   // chiedere all'utente la sua età
   var eta = prompt("inserisci la tua età");
-  console.log("età ",eta)
+  console.log("età ",eta);
 
 // OPERAZIONI
-  // approssimazione
-  costoBase = (0.21 * km).toFixed(2);
-  console.log("costo base",costoBase)
+  // costo di base
+  costoBase = (0.21 * km);
+  console.log("costo base",costoBase);
 
   // sconti
+  scontoUno = ((costoBase * 20) / 100);
+  scontoDue = ((costoBase * 40) / 100);
+  console.log("sconti:",scontoUno, scontoDue);
 
+
+  if (eta < 18) {
+    // se l'utente è minorenne
+    costoTot = costoBase - scontoUno;
+  } else if (eta > 65) {
+    // se l'utente è over 65
+    costoTot = costoBase - scontoDue;
+  } else {
+    // se l'utente è tra i 19 e i 64 anni
+    costoTot = costoBase;
+  }
+  console.log("costo del biglietto ",costoTot.toFixed(2));
 
 // OUTPUT
   // dire all'utente il costo del biglietto
